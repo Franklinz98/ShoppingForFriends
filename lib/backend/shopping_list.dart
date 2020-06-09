d3b544790c3de7800bde728af5cb4649db76b13c
DavidCuentasMar <davidandrescuentasmar@gmail.com>
2020-06-09 08:17:20 -0500

    delete/update/futures shoppinglist

--- lib/backend/shopping_list.dart
+++ lib/backend/shopping_list.dart
@@ -1,17 +1,51 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-class ShoppingList{
-    createShoppingList(String userId, shoppingListMap){
-      Firestore.instance.collection('ShoppingList').document(userId).setData(shoppingListMap).catchError((e){
+class ShoppingList {
+  Future<bool> createShoppingList(String userId, shoppingListMap) async{
+    Firestore.instance
+        .collection('ShoppingList')
+        .document(userId)
+        .setData(shoppingListMap)
+        .catchError((e) {
       print(e.toString());
     });
-    }
-    
-    setShoppingListPublic(String userId)async{
-      return await Firestore.instance.collection("ShoppingList").document(userId).updateData({'isPublic': true});
-    }
+    return true;
+  }
 
-    getPublicShoppingLists() async{
-    return await Firestore.instance.collection("ShoppingList").where('isPublic', isEqualTo: true).snapshots();
+  Future<bool> updateList(String userId, shoppingListMap) async {
+    await Firestore.instance
+        .collection('ShoppingList')
+        .document(userId)
+        .updateData(shoppingListMap)
+        .catchError((error) {
+      print("error: $error");
+    });
+    return true;
+  }
+
+  Future<bool> deteleList(String userId) async {
+    await Firestore.instance
+        .collection('ShoppingList')
+        .document(userId)
+        .delete()
+        .catchError((error) {
+      print("error: $error");
+    });
+    return true;
+  }
+
+  Future<bool> setShoppingListPublic(String userId) async {
+    Firestore.instance
+        .collection("ShoppingList")
+        .document(userId)
+        .updateData({'isPublic': true});
+    return true;
+  }
+
+  getPublicShoppingLists() async {
+    return await Firestore.instance
+        .collection("ShoppingList")
+        .where('isPublic', isEqualTo: true)
+        .snapshots();
   }
 }
